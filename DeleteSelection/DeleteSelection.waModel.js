51e4d2e38533e978bbbf50129ed44a75203d9db2
David Robbins <dave@wakanda.org>
2012-08-27 15:26:09 -0700

    initial creation of this repository

---
+++ DeleteSelection/DeleteSelection.waModel.js
@@ -0,0 +1,58 @@
+﻿
+guidedModel =// @startlock
+{
+	Company :
+	{
+		events :
+		{
+			onRemove:function()
+			{// @endlock
+				var sessionRef = currentSession(); // Get session.
+				if (!sessionRef.belongsTo("Admin")) {
+					//Don't delete a company with employees unless you are Admin.
+					if (this.employees.length > 0) {
+						return {error: 100, errorMessage: "You cannot delete a Company that still has employees."};
+					}
+				}
+			}// @startlock
+		},
+		collectionMethods :
+		{// @endlock
+			getIDs:function()
+			{// @lock
+				var companyIDs = this.toArray("ID");
+				return companyIDs;
+			},// @lock
+			deleteSelected:function(theSelectedRows)
+			{// @lock
+				//this contains our current company collection.
+				var currentCompanyCollection = this;
+				//Create two empty collections.
+				var deleteCollection = ds.Company.createEntityCollection(); 
+				var xDeleteCollection = ds.Company.createEntityCollection();
+				//Iterate through the selected rows passed in from the client
+				//to populate the delete collection.
+				theSelectedRows.forEach(function(rowNum) { //create delete collection
+					deleteCollection.add(currentCompanyCollection[rowNum]);
+				});
+				//Remove selected companies - if rejected add to our 
+				// xDelete collection.	
+				theSelectedRows.forEach(function(rowNum) {
+					okToAddToDeleteCollection = true;
+					try
+					{
+					    currentCompanyCollection[rowNum].remove();
+					}
+					catch (err)
+					{
+					    xDeleteCollection.add(currentCompanyCollection[rowNum]);
+					}
+				});
+				//The delete collection minus the rejected delete requests.
+				var validDeleteCollection = deleteCollection.minus(xDeleteCollection);
+				//Return our updated company collection.
+				return currentCompanyCollection.minus(validDeleteCollection);	
+			}// @startlock
+		}
+	}
+};// @endlock
---
+++ DeleteSelection/WebFolder/scripts/index.js
@@ -0,0 +1,69 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var dataGrid1 = {};	// @dataGrid
+	var button4 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	dataGrid1.onRowClick = function dataGrid1_onRowClick (event)// @startlock
+	{// @endlock
+		$$('richText4').setValue("");
+	};// @lock
+
+	button4.click = function button4_click (event)// @startlock
+	{// @endlock
+		//Keep an array of the IDs of the companies to delete.
+		var companyIDs = WAF.sources.company.getIDs(); 
+		//In the callback of the deleteSelected method we will see if any of these IDs 
+		//   are returned in the collection and highlight them and inform the user.
+		var selectedEntityIndexArray = $$('dataGrid1').getSelectedRows(); 
+		var selectedCompanyIDs = []; 
+		$.each(selectedEntityIndexArray, function() { 
+			selectedCompanyIDs.push(companyIDs[this].ID);
+		});
+		
+		WAF.sources.company.deleteSelected({
+			onSuccess: function(evt) {
+				WAF.sources.company.setEntityCollection(evt.result);
+				//Highlight Rows of Selected Companies that could not be deleted.
+				/**/
+				errorRows = [];
+				var companyIDs = WAF.sources.company.getIDs();
+				for (var i = 0, len = companyIDs.length; i < len; i++) {
+					$.each(evt.userData.selectedCompanyIDs, function() {
+						if (this == companyIDs[i].ID) {
+							errorRows.push(i);
+						}
+					});
+				}
+				
+				//If we found some rows that did not get deleted select them
+				//   and diplay message to the user.
+				/**/
+				if (errorRows.length > 0) { 
+					$$('richText4').setValue("The highlighted companies could not be removed."); 
+					
+					
+					WAF.sources.company.selectByKey(companyIDs[errorRows[0]].ID);
+					$$('dataGrid1').setSelectedRows(errorRows);
+					
+					
+					
+				} else {
+					$$('richText4').setValue("Your selection has been removed on the server.");
+					$$('dataGrid1').setSelectedRows();
+				}
+				
+			},
+			userData: {selectedCompanyIDs: selectedCompanyIDs}
+		},$$('dataGrid1').getSelectedRows());
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("dataGrid1", "onRowClick", dataGrid1.onRowClick, "WAF");
+	WAF.addListener("button4", "click", button4.click, "WAF");
+// @endregion
+};// @endlock
